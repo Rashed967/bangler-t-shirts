6131ae6226a8f38b1911ae0d3f68af62667d61cf
Rashed967 <rashedhasan967.com>
2023-04-08 00:50:51 +0600

    added something

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = ({cart, removeFromeCart}) => {
     const {name, price, _id} = cart
+    let message;
+    // if(cart.length === 0){
+    //     message = <p>Please add some product</p>
+    // }
+    // else{
+    //     message = <p>Thanks for adding product</p>
+    // }
     return (
         <div>
+            {/* {cart.length === 0 ? <p>add product</p>  : <p>thanks</p> } */}
+            {cart.length !== 0 && <p className={`blue ${cart.length > 2 && 'yellow'} ${cart.length > 4 && 'green'}`}>Thanks</p>}
+            {cart.length > 0 || <p>Add products</p>}
            {
             cart.map(tshirt => <p
                 key={tshirt._id}
